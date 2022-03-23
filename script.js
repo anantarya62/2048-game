6c9d9dd23574548c89bc2a10178f5306e19bfbe2
Rohan Karn <karn.rohan1@gmail.com>
2022-03-23 18:03:15 +0530

    cleaned code

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ import Tile from "./Tile.js";
 
 const gameBoard = document.getElementById("game-board");
 const grid = new Grid(gameBoard);
-console.log(grid.randomEmptyCell());
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 setupInput();
